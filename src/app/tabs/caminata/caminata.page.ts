27db33138b498273c4edd8777c1bdadb5a99502b
Neil Vela Marchena <neil_vela@usmp.pe>
2023-10-09 22:43:34 -0500

    boton de control de inicio de caminata

--- src/app/tabs/caminata/caminata.page.ts
+++ src/app/tabs/caminata/caminata.page.ts
@@ -1,5 +1,5 @@
-
 import { Component, OnInit, OnDestroy } from '@angular/core';
+
 @Component({
   selector: 'app-caminata',
   templateUrl: './caminata.page.html',
@@ -8,29 +8,55 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export class CaminataPage implements OnInit, OnDestroy {
   stepCount: number = 0;
   private isListening: boolean = false;
-  constructor() { }
+  private deviceMotionHandler: (event: DeviceMotionEvent) => void;
+
+  constructor() {
+
+    this.deviceMotionHandler = (event: DeviceMotionEvent) => {};
+  }
 
   ngOnInit() {
-    if ('DeviceMotionEvent' in window) {
-      window.addEventListener('devicemotion', this.handleDeviceMotion.bind(this));
+    this.deviceMotionHandler = this.handleDeviceMotion.bind(this);
+  }
+
+  ngOnDestroy() {
+    this.stopListening();
+  }
+
+  inicioFin() {
+    if (this.isListening) {
+      this.stopListening();
+    } else {
+      this.startListening();
+    }
+  }
+
+  startListening() {
+    if ('DeviceMotionEvent' in window && !this.isListening) {
+      window.addEventListener('devicemotion', this.deviceMotionHandler);
       this.isListening = true;
     } else {
-      console.error('Device Motion not supported on this device.');
+      console.error('Device Motion not supported on this device or already listening.');
     }
   }
-  ngOnDestroy() {
+
+  stopListening() {
     if (this.isListening) {
-      window.removeEventListener('devicemotion', this.handleDeviceMotion.bind(this));
+      window.removeEventListener('devicemotion', this.deviceMotionHandler);
+      this.isListening = false;
     }
   }
+
   handleDeviceMotion(event: DeviceMotionEvent) {
     if (event.accelerationIncludingGravity) {
       const z = event.accelerationIncludingGravity.z;
       if (z !== null && !isNaN(z) && z > 9.81) {
-        // Aumentar el contador de pasos cuando se detecta un paso
         this.stepCount++;
       }
     }
   }
 
+  resetStepCount() {
+    this.stepCount = 0;
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  googleMapsApiKey: 'AIzaSyD_H5cyWMb3pkmTyx_2Oz-Lm4tYoj5cu4k',
 };
 
 /*
